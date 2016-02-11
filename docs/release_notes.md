66e8cc64aa167794a7ec32ba3e642928affe687c
Jason <jasonrfarkas@gmail.com>
2016-02-11 12:05:45 -0500

    chore(setup): create base django project.

---
+++ docs/release_notes.md
@@ -0,0 +1,16 @@
+# Release Notes
+
+## [0.1.0-dev]
+
+__Date:__ [Under Developement](https://github.com/jasonrfarkas/hello_world_django_cookies/issues/1)
+
+__Features__
+
+- ...
+- initial setup of project.
+
+__Fixes__
+
+- ...
+
+[0.1.0-dev]: https://github.com/jasonrfarkas/hello_world_django_cookies/compare/v0.0.0...master
---
+++ hello_world/urls.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+'''Root url routering file.
+
+You should put the url config in their respective app putting only a
+refernce to them here.
+'''
+from __future__ import absolute_import, unicode_literals
+
+# Third Party Stuff
+from django.conf import settings
+from django.conf.urls import include, url
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.views import defaults as dj_default_views
+
+# Hello_World Stuff
+from hello_world.base import views as base_views
+
+from .routers import router
+
+handler500 = base_views.server_error
+
+urlpatterns = [
+
+    url(r'^(?P<filename>(robots.txt)|(humans.txt))$',
+        base_views.root_txt_files, name='root-txt-files'),
+
+    # Rest API
+    url(r'^api/', include(router.urls)),
+
+    # Browsable API
+    url(r'^api/auth-n/', include('rest_framework.urls', namespace='rest_framework')),
+
+    # Django Admin
+    url(r'^admin/', include(admin.site.urls)),
+
+    # pages/ landing pages
+    url(r'^', include("hello_world.pages.urls", namespace="pages")),
+
+    # Your stuff: custom urls go here
+
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += [
+        url(r'^400/$', dj_default_views.bad_request, kwargs={'exception': Exception("Bad Request!")}),
+        url(r'^403/$', dj_default_views.permission_denied, kwargs={'exception': Exception("Permission Denied!")}),
+        url(r'^404/$', dj_default_views.page_not_found, kwargs={'exception': Exception("Not Found!")}),
+        url(r'^500/$', handler500),
+    ]
